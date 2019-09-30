f4c36af477d1ecfde1a31f61af8321e9b247d14d
Hugo Quezada <hugh.quezada8@gmail.com>
2019-09-30 10:57:08 -0500

    Adding configs to use eureka

--- src/main/java/com/hq/SpringBootProducerApplication.java
+++ src/main/java/com/hq/SpringBootProducerApplication.java
@@ -2,11 +2,13 @@ package com.hq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class SpringBootHelloWorldApplication {
+@EnableDiscoveryClient
+public class SpringBootProducerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
+		SpringApplication.run(SpringBootProducerApplication.class, args);
 	}
 }
